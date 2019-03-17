cae462fb7d25ccb1202e528dd4f73962eb5b0c6f
Mac McCann <johnwilfredmccann@gmail.com>
2019-03-17 17:37:29 -0400

    added exec wrapper

---
+++ exec.go
@@ -0,0 +1,14 @@
+package fmtwrapper
+
+import "os"
+
+func Exec(name string, arg ...string) string {
+	cmd := exec.Command(name, arg)
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	err := cmd.Run()
+	if err != nil {
+		return err
+	}
+	return out.String()
+}
